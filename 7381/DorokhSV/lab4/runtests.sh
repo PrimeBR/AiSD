a95898a15321839bad531e171a42e9e20e31a94b
PrimeBR <sdorox@mail.ru>
2018-11-08 17:53:08 +0300

    report added

--- 7381/DorokhSV/lab4/runtests.sh
+++ 7381/DorokhSV/lab4/runtests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo -n > result.txt
-echo "Examples of incorrect input: "
+echo "Examples of correct input: "
 echo "Test 1: (a(b(d(g##)(e##))(e(d##)(p##)))(c#(g(e##)#)))"
 echo "Test 1: (a(b(d(g##)(e##))(e(d##)(p##)))(c#(g(e##)#)))" >> result.txt
 ./lab4 < Tests/test1.txt >> result.txt
@@ -10,7 +10,7 @@ echo "\nTest 2: (a(b##)#)" >> result.txt
 echo "Test 3: (a (b#    #)(c#            (k##)))"
 echo "\nTest 3: (a (b#    #)(c#            (k##)))" >> result.txt
 ./lab4 < Tests/test3.txt >> result.txt
-echo "Examples of correct input: "
+echo "Examples of incorrect input: "
 echo "Test 4: ()"
 echo "\nTest 4: ()" >> result.txt
 ./lab4 < Tests/test4.txt >> result.txt
